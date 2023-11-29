69a4e94f8d2dda965b64fab71cdfc65b6bf33850
OlexandrJ <134488937+OlexandrJ@users.noreply.github.com>
2023-11-29 12:03:10 +0200

    summ

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
-export const App = () => {
+// App.jsx
+import React, { useState } from 'react';
+import FeedbackOptions from './FeedbackOptions';
+import Statistics from './Statistics';
+import Section from './Section';
+
+const App = () => {
+  const [state, setState] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0
+  });
+
+  const handleFeedback = (type) => {
+    setState((prev) => ({ ...prev, [type]: prev[type] + 1 }));
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1>Cafe Expresso - Collecting reviews</h1>
+      <Section title="Please leave feedback">
+        <FeedbackOptions options={['good', 'neutral', 'bad']} onLeaveFeedback={handleFeedback} />
+      </Section>
+      {state.good + state.neutral + state.bad > 0 ? (
+        <Section title="Statistics">
+          <Statistics good={state.good} neutral={state.neutral} bad={state.bad} />
+        </Section>
+      ) : (
+        <p>No feedback given</p>
+      )}
     </div>
   );
 };
+
+export default App;
